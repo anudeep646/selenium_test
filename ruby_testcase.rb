0d1308c01d03e3f9586d3d551b7f431f3f5d8988
anudeep646 <anudeep646@gmail.com>
2017-08-15 22:13:12 -0700

    Create ruby_testcase.rb

---
+++ ruby_testcase.rb
@@ -0,0 +1,29 @@
+require 'selenium-webdriver'
+require 'rspec/expectations'
+include RSpec::Matchers
+
+def setup
+  caps = Selenium::WebDriver::Remote::Capabilities.send("chrome")
+  @driver = Selenium::WebDriver.for(:remote, url: "http://0.0.0.0:4444/wd/hub", desired_capabilities: caps)
+  @driver.manage.window.size = Selenium::WebDriver::Dimension.new(1920, 1080)
+end
+
+def teardown
+  @driver.quit
+end
+
+def run
+  setup
+  yield
+  teardown
+end
+
+run do
+  # Open the main page and check for the title
+  @driver.get 'https://www.walmart.com/'
+  expect(@driver.title).to eql 'Walmart.com: Free 2-Day Shipping on Millions of Items'
+
+  # Generate a screenshot of the main page
+  @driver.save_screenshot(File.join(Dir.pwd, "selium-docker-main-page.png"))
+  
+  end
